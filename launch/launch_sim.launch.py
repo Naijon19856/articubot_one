3849bac1fd3dbea0367b31ef3a7228b2ff1309a4
Naim <muhammad_naim1985@yahoo.com>
2025-08-29 18:51:48 +0800

    Created core URDF

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,20 +4,19 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
 
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='articubot_one' #<--- CHANGE ME
+    # Include the robot_state_publisher launch file, provided by our own package.
+    # Force sim time to be enabled
+    package_name='articubot_one' 
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -31,17 +30,41 @@ def generate_launch_description():
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
              )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    # Run the spawner node from the gazebo_ros package to spawn the robot model
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                                   '-entity', 'articubot_one'],
                         output='screen')
 
 
+    # Spawner for the differential drive controller
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
 
-    # Launch them all!
+    # Spawner for the joint state broadcaster
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
+
+    # Launch all nodes, but use a handler to wait for the spawn_entity process to finish
+    # before launching the controllers.
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+
+        # This event handler will start the controller spawners only after the
+        # spawn_entity node has completed its work and exited.
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[diff_drive_spawner, joint_broad_spawner]
+            )
+        )
     ])
